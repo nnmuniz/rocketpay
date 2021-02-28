3584e6c2964474e415ae2ae93bf66158dd8fc07f
Nathalya Muniz <pcnathalya@hotmail.com>
2021-02-28 16:43:28 -0300

    Incluir testes unitários automatizados

--- lib/rocketpay/numbers.ex
+++ lib/rocketpay/numbers.ex
@@ -5,6 +5,26 @@ defmodule Rocketpay.Numbers do
     |> handle_file()
   end
 
-  defp handle_file({:ok, file}), do: file
-  defp handle_file({:error, reason}), do: {:error, "Invalid File"}
+  # defp handle_file({:ok, result}) do
+  #   result =
+  #   result = String.split(result, ",")
+  #   result = Enum.map(result, fn number -> String.to_integer(number) end)
+  #   result = Enum.sum(result)
+  #   result
+  # end
+
+  # Linguagem imutável por isso é necessário reatribuir o valor da variável,
+  # Pipe Operator compondo as funções e agregando resultado.
+
+  defp handle_file({:ok, result}) do
+    result =
+    result
+    |> String.split(",")
+    |> Stream.map(fn number -> String.to_integer(number) end)
+    |> Enum.sum()
+
+    {:ok, %{result: result}}
+  end
+
+  defp handle_file({:error, _reason}), do: {:error, %{message: "Invalid file!"}}
 end
