a7ad9456d4724c4e201f00ff6c3bf85b4de44ecc
Nathalya Muniz <pcnathalya@hotmail.com>
2021-03-16 14:53:39 -0300

    Lindando com erros com o Fallback Controller

--- lib/rocketpay_web/controllers/users_controller.ex
+++ lib/rocketpay_web/controllers/users_controller.ex
@@ -3,22 +3,13 @@ defmodule RocketpayWeb.UsersController do
 
   alias Rocketpay.User
 
+  action_fallback RocketpayWeb.FallbackController
+
   def create(conn, params) do
-    params
-    |> Rocketpay.create_user()
-    |> handle_response(conn)
-  end
-
-  defp handle_response({:ok, %User{} = user}, conn) do
+    with {:ok, %User{} = user} <- Rocketpay.create_user(params) do
     conn
-    |> put_status(:created)
+    |> put_status(:ok)
     |> render("create.json", user: user)
-  end
-
-  defp handle_response({:error, result}, conn) do
-    conn
-    |> put_status(:bad_request)
-    |> put_view(RocketpayWeb.ErrorView)
-    |> render("400.json", result: result)
+    end
   end
 end
