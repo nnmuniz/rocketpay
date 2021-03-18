2d9ef54078f3b10cda3c1b0c7dc85d04726ad9eb
Nathalya Muniz <pcnathalya@hotmail.com>
2021-03-18 01:50:31 -0300

    Struct, realizando depósitos: inserir ou atualizar dados da tabela

--- lib/rocketpay_web/controllers/users_controller.ex
+++ lib/rocketpay_web/controllers/users_controller.ex
@@ -8,7 +8,7 @@ defmodule RocketpayWeb.UsersController do
   def create(conn, params) do
     with {:ok, %User{} = user} <- Rocketpay.create_user(params) do
     conn
-    |> put_status(:ok)
+    |> put_status(:created)
     |> render("create.json", user: user)
     end
   end
--- lib/rocketpay_web/router.ex
+++ lib/rocketpay_web/router.ex
@@ -11,6 +11,11 @@ defmodule RocketpayWeb.Router do
     get "/:filename", WelcomeController, :index
 
     post "/users", UsersController, :create
+
+    post "/accounts/:id/deposit", AccountsController, :deposit
+
+    post "/accounts/:id/withdraw", AccountsController, :withdraw
+
   end
 
   # Enables LiveDashboard only for development
